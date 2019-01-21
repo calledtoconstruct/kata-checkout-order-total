798e1c34f62e255cca33f9166e3a9c8dd6c241c2
Joseph Woolley <joe@calledtoconstruct.net>
2019-01-21 15:39:39 -0500

    Feature: Discounts - adding bulk flat price discount.

--- typescript/src/discount.ts
+++ typescript/src/discount.ts
@@ -14,6 +14,16 @@ export class StandardDiscount implements Discount {
     ) {}
 }
 
+export class BulkFlatPriceDiscount implements Discount {
+    constructor(
+        public readonly startDate: Date,
+        public readonly endDate: Date,
+        public readonly code: string,
+        public readonly quantity: number,
+        public readonly price: number
+    ) {}
+}
+
 export class DiscountList {
     
     public add(discount: Discount): void {
--- typescript/test/test.spec.ts
+++ typescript/test/test.spec.ts
@@ -1,7 +1,7 @@
 /* global describe, it, expect, beforeEach */
 
 import { ItemList, Item } from '../src/item';
-import { Discount, StandardDiscount, DiscountList } from '../src/discount';
+import { Discount, StandardDiscount, DiscountList, BulkFlatPriceDiscount } from '../src/discount';
 
 interface Parameterized<T> {
     it(description: string, func: (value: T) => void): void;
@@ -118,6 +118,7 @@ describe('Given a collection of Pricing Rules', () => {
 
     const validDiscounts: Parameters<[string, Discount]> = new Parameters<[string, Discount]>([
         ['standard discount', new StandardDiscount(new Date(), new Date(), 'random item code', 1.0)],
+        ['bulk flat price', new BulkFlatPriceDiscount(new Date(), new Date(), 'random item code', 3, 5.0)],
     ]);
 
     validDiscounts.forEach().describe('And a valid discount When adding it', (item: [string, Discount]) => {
