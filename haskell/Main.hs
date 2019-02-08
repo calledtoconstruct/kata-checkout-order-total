ff0dd5bed4517e06bd5db70a146ab03bc5c1518d
Joseph Woolley <joe@enlighten.ment.today>
2019-02-07 22:44:30 -0500

    Clean-up: fix formatting and alignment.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -67,24 +67,24 @@ module Main where
   }
 
   add :: DiscountList -> ItemList -> TransactionType -> [Item] -> IO Double
-  add discountList itemList transaction []      = do
+  add discountList itemList transaction []            = do
     putStrLn "Item not found!"
     prompt discountList itemList transaction
-  add discountList itemList transaction [item]  = do
+  add discountList itemList transaction [item]        = do
     let nextTransaction = scanItem transaction (getDiscount discountList) item
     putStrLn $ show $ transactionTotal nextTransaction
     prompt discountList itemList nextTransaction
 
   prompt :: DiscountList -> ItemList -> TransactionType -> IO Double
-  prompt discountList itemList transaction = do
+  prompt discountList itemList transaction            = do
     nextCode <- putStr "Enter a product code: " *> getLine
     waitForScan discountList itemList transaction nextCode
 
   scan :: DiscountList -> ItemList -> TransactionType -> String -> IO Double
-  scan discountList itemList transaction code = add discountList itemList transaction (getItem itemList code)
+  scan discountList itemList transaction code         = add discountList itemList transaction (getItem itemList code)
 
   waitForScan :: DiscountList -> ItemList -> TransactionType -> String -> IO Double
-  waitForScan discountList itemList transaction code = case code of
+  waitForScan discountList itemList transaction code  = case code of
     "done"              -> return $ transactionTotal transaction
     otherwise           -> scan discountList itemList transaction code
 
