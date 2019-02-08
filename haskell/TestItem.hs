6bca648db2a0d356cb2c26b040d912aacbcabdbe
Joseph Woolley <joe@enlighten.ment.today>
2019-02-07 22:27:25 -0500

    Feature: adding serialization test for item.

--- haskell/TestItem.hs
+++ haskell/TestItem.hs
@@ -3,13 +3,25 @@
 
 module TestItem where
 
-  import Data.Aeson (decode)
+  import Data.Aeson (encode, decode)
   import Data.ByteString.Lazy.UTF8 (fromString)
   import Item
 
+  dogFood :: Item
+  dogFood = ByQuantityItem {
+    itemCode = "dog food",
+    itemDescription = "2 lbs bag of dry dog food.",
+    itemType = "by quantity",
+    itemPrice = 9.99
+  }
+
   -- $setup
-  -- >>> dogFood = fromString "{\"tag\":\"ByQuantityItem\",\"itemType\":\"by quantity\",\"itemCode\":\"dog food\",\"itemDescription\":\"2 lbs bag of dry dog food.\",\"itemPrice\":9.99}"
+  -- >>> dogFoodJSON = fromString "{\"tag\":\"ByQuantityItem\",\"itemType\":\"by quantity\",\"itemCode\":\"dog food\",\"itemDescription\":\"2 lbs bag of dry dog food.\",\"itemPrice\":9.99}"
+
+  -- | Serialization of Standard Item
+  -- >>> encode dogFood
+  -- "{\"tag\":\"ByQuantityItem\",\"itemType\":\"by quantity\",\"itemCode\":\"dog food\",\"itemDescription\":\"2 lbs bag of dry dog food.\",\"itemPrice\":9.99}"
 
   -- | Deserialization of Standard Item
-  -- >>> case (decode dogFood) :: Maybe Item of Just item -> item; Nothing -> error "Invalid input"
+  -- >>> case (decode dogFoodJSON) :: Maybe Item of Just item -> item; Nothing -> error "Invalid input"
   -- ByQuantityItem {itemCode = "dog food", itemDescription = "2 lbs bag of dry dog food.", itemType = "by quantity", itemPrice = 9.99}
