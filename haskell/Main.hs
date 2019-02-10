3331ee9193eb1a31117799b729c7c62325a1a429
Joseph Woolley <joe@enlighten.ment.today>
2019-02-10 16:17:08 -0500

    Refactor: load discounts from disk for console.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -7,65 +7,6 @@ module Main where
   import ItemList
   import DiscountList
 
-  discountDogFood :: Discount
-  discountDogFood = StandardDiscount {
-    discountCode = "dog food",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountPrice = 3.00
-  }
-
-  discountCrisps :: Discount
-  discountCrisps = BulkFlatPriceDiscount {
-    discountCode = "crisps",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountBulk = 3,
-    discountPrice = 1.00
-  }
-
-  discountCatFood :: Discount
-  discountCatFood = UpSalePercentDiscount {
-    discountCode = "cat food",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountBulk = 2,
-    discountSale = 1,
-    discountPercent = 0.50
-  }
-
-  discountTurkey :: Discount
-  discountTurkey = LimitedUpSalePercentDiscount {
-    discountCode = "turkey",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountBulk = 2,
-    discountSale = 1,
-    discountPercent = 1.0,
-    discountLimit = 6
-  }
-
-  discountSwissCheese :: Discount
-  discountSwissCheese = UpSaleFlatPriceDiscount {
-    discountCode = "swiss cheese",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountBulk = 2,
-    discountSale = 1,
-    discountPrice = 1.0
-  }
-
-  discountBologna :: Discount
-  discountBologna = LimitedUpSaleFlatPriceDiscount {
-    discountCode = "bologna",
-    discountStartDate = fromGregorian 2019 1 1,
-    discountEndDate = fromGregorian 2019 12 31,
-    discountBulk = 2,
-    discountSale = 1,
-    discountPrice = 1.5,
-    discountLimit = 6
-  }
-
   add :: DiscountList -> ItemList -> TransactionType -> [Item] -> IO Double
   add discountList itemList transaction []            = do
     putStrLn "Item not found!"
@@ -88,8 +29,8 @@ module Main where
     "done"              -> return $ transactionTotal transaction
     otherwise           -> scan discountList itemList transaction code
 
-  console :: ItemList -> IO ()
-  console itemList
+  console :: DiscountList -> ItemList -> IO ()
+  console discountList itemList
     | invalidDiscount   = print "Invalid Discount"
     | invalidItem       = print "Invalid Item"
     | otherwise         = do
@@ -98,9 +39,9 @@ module Main where
     where invalidDiscount = any (==False) $ flip isValidDiscount (getItem itemList) <$> (transactionDiscounts transaction)
           invalidItem     = any (==False) $ isValidItem <$> transactionItems transaction
           transaction     = createTransaction $ fromGregorian 2019 4 30
-          discountList    = addDiscounts createDiscountList (getItem itemList) $ discountDogFood: discountCrisps: discountCatFood: discountTurkey: discountSwissCheese: discountBologna: []
 
   main :: IO ()
   main = do
     itemList            <- loadItems "./Items.json" $ pure createItemList
-    console itemList
+    discountList        <- loadDiscounts (getItem itemList) "./Discounts.json" $ pure createDiscountList
+    console discountList itemList
