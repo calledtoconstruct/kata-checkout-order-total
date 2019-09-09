72d17c8d161d61d6f4b552d1136169012a153a3e
Joseph <joe@calledtoconstruct.net>
2019-09-09 03:39:25 +0000

    Cleaning up docker images.

--- typescript/build.sh
+++ typescript/build.sh
@@ -2,8 +2,11 @@ dockenv="eval $(sudo minikube docker-env)"
 dockdo="sudo docker --host="$DOCKER_HOST" --tlsverify=1 --tlscacert=\"$DOCKER_CERT_PATH/ca.pem\" --tlscert=\"$DOCKER_CERT_PATH/cert.pem\" --tlskey=\"$DOCKER_CERT_PATH/key.pem\""
 $dockenv
 $dockdo rmi item-api:v1.0.0
+cat Dockerfile.base Dockerfile.item-api > Dockerfile
+$dockdo build . --tag item-api:v1.0.0
 $dockdo rmi discount-api:v1.0.0
+cat Dockerfile.base Dockerfile.discount-api > Dockerfile
+$dockdo build . --tag discount-api:v1.0.0
 $dockdo rmi application:v1.0.0
-$dockdo build . --file item-api.Dockerfile --tag item-api:v1.0.0
-$dockdo build . --file discount-api.Dockerfile --tag discount-api:v1.0.0
-$dockdo build . --file application.Dockerfile --tag application:v1.0.0
+cat Dockerfile.base Dockerfile.application > Dockerfile
+$dockdo build . --tag application:v1.0.0
