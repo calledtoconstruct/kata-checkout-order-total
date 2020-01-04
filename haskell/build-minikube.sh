00c2419e0edbc62016caac6f0cbb18280731a6ca
calledtoconstruct <calledtoconstruct@gmail.com>
2020-01-04 14:24:03 -0500

    Upgrade minikube build.

--- haskell/build-minikube.sh
+++ haskell/build-minikube.sh
@@ -1,12 +1,7 @@
 dockenv="eval $(sudo minikube docker-env)"
 dockdo="sudo docker --host="$DOCKER_HOST" --tlsverify=1 --tlscacert=\"$DOCKER_CERT_PATH/ca.pem\" --tlscert=\"$DOCKER_CERT_PATH/cert.pem\" --tlskey=\"$DOCKER_CERT_PATH/key.pem\""
 $dockenv
-$dockdo build .
 $dockdo rmi item-api:v1.0.0
 $dockdo rmi discount-api:v1.0.0
-image_name=$($dockdo images | grep -P "^(\<none\>\s*){2}([a-z0-9]*).*$" | tail -1)
-image_id=`expr match "$image_name" "^<none>\s*<none>\s*\([a-z0-9]*\).*$"`
-$dockdo tag $image_id item-api:v1.0.0
-image_name=$($dockdo images | grep -P "^(\<none\>\s*){2}([a-z0-9]*).*$" | tail -1)
-image_id=`expr match "$image_name" "^<none>\s*<none>\s*\([a-z0-9]*\).*$"`
-$dockdo tag $image_id discount-api:v1.0.0
+$dockdo build --tag item-api:v1.0.0 --target item-api .
+$dockdo build --tag discount-api:v1.0.0 --target discount-api .
